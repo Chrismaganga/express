dc754311294856f36ea0b042e4dbfd7da9c6d20b
Chrismaganga <chrismaganga3@gmail.com>
2023-05-30 20:06:23 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+
+
+
+const App = () => {
+  return (
+    < >
+      <Navbar/>
+      <Home/>
+      </>
+  )
+}
+
+export default App
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const container = styled.div`
+height: 60px;
+back
+`
+
+
+const Navbar = () => {
+  return (
+    <div className="container">
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
