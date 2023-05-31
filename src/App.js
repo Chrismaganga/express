b6555033514bdeb6da98fc6d1b9ea4a76903ec46
Chrismaganga <chrismaganga3@gmail.com>
2023-05-31 20:47:16 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import Announcement from "./components/Announcement"
 import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
+import Slider from "./components/Slider"
+
 
 
 
 const App = () => {
   return (
     < >
+    <Announcement/>
       <Navbar/>
-      <Home/>
+      <Slider/>
+    
       </>
   )
 }
