26318622216d04a9d2575f8397144d5e8cd50e90
Chrismaganga <chrismaganga3@gmail.com>
2023-07-26 23:39:00 +0200

    update

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'x3j6ison',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/product.ts
@@ -0,0 +1,44 @@
+export default {
+    name: "Product",
+    type: "object",
+    fields: [
+      {
+        name: "id",
+        type: "string",
+        price: "string",
+        category: "string",
+      },
+      {
+        name: "created",
+        type: "number",
+      },
+      {
+        name: "last_updated",
+        type: "number",
+      },
+      {
+        name: "active",
+        type: "boolean",
+      },
+      {
+        name: "permalink",
+        type: "string",
+      },
+      {
+        name: "name",
+        type: "string",
+      },
+      {
+        name: "description",
+        type: "string",
+      },
+      {
+        name: "price",
+        type: "number",
+      },
+      {
+        name: "quantity",
+        type: "number",
+      },
+    ],
+   };
